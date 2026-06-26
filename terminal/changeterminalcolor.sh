242f78a81f3617d0e0484762e455500e91570ddb
Nicoleta Iancu <nicoleta.v.iancu@gmail.com>
2026-06-26 23:06:32 +0300

    added terminal change of colors

---
+++ terminal/changeterminalcolor.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+git clone https://github.com/dracula/gnome-terminal.git
+cd gnome-terminal
+./install.sh
+echo "DONE!!!!!"
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~        
